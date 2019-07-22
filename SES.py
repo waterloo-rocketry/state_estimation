aa38cdf28b498addba3a6b9526c52cb6bda936d0
dwkletke <45055161+dwkletke@users.noreply.github.com>
2019-07-22 13:41:04 -0400

    Add files via upload

---
+++ SES.py
@@ -0,0 +1,117 @@
+import numpy as np
+import rocket_math as rm
+# This file controls the main functionality of the State Estimation Simulator
+'''
+Simulator inputs:
+    Rate/timestep
+    Rocket parameters: mass, average thrust, burn time. If we decide to implement a more sophisticated or flexible rocket model, we can add more parameters here, but I think this is the minimum we'd want and good enough to start with.
+    Sensor models: noise, sensitivity, bias, hysteresis, etc. Any information from that datasheet that we need to account for when transforming our ground truth data into sensor measurements. Again, we can start off simple here (noise only) and add more parameters as we go.
+    Other parameters: launch tower angle, launch site altitude (we need this for later pressure calculations), local magnetic field data. These are the first ones I thought of, but there may be more later.
+Simulator outputs:
+    A "ground truth" file with ground truth simulated data (no sensor models) for each timestep. To start, we definitely want the following simulated:
+        Absolute position (ENU)
+        Velocity
+        Acceleration
+        Orientation (another post to follow with regards to rotation representations)
+    A "sensor data" file with the simulated sensor measurements for each timestep. Right now, we need:
+        Barometric pressure
+        Temperature
+        Acceleration (x, y, z)
+        Angular rates (x, y, z)
+        Magnetic field (B field) components (x, y, z)
+Implementation:
+    We can likely make the following assumptions to make initial implementation easier:
+        The rocket's yaw rate is constant and zero.
+        The rocket's pitch rate is constant and relatively small (~1 degree/s).
+        The angle of attack is zero.
+    At each timestep, the simulator should do the following:
+        Recall the previous state of the rocket: position, orientation, velocity, angular velocity.
+        Determine the forces acting on the rocket: gravity, thrust (if the engine is still burning), drag.
+        Determine the acceleration of the rocket (F = ma).
+        Update the rocket state:
+            Integrate velocity to find the new position of the rocket.
+            Integrate acceleration to find the new velocity of the rocket.
+            Update the orientation of the rocket (details will depend on how we choose to represent orientation).
+        Output the rocket state to the ground truth file.
+        Convert ground truth rocket state to ground truth sensor measurements:
+            From rocket and launch site altitude, determine barometric pressure and temperature.
+            From orientation and known pitch/roll rates, determine measured angular velocities.
+            From orientation and known absolute acceleration, determine accelerometer measurements.
+            From orientation and local magnetic field data, determine magnetometer measurements.
+        Add sensor errors: noise, bias, etc.
+        Output sensor data to the sensor data file.
+'''
+
+# The rocket's fixed angular velocities
+yaw_rate = 0
+pitch_rate = 1
+roll_rate = 180  # in deg/s
+
+# Launch site parameters
+tower_angle = 90
+launch_site_altitude = 0
+local_magnetic_field = 0  # TODO: figure out what we need to actually store here
+
+# Timestep setup
+timestep = 0.01
+end_time = 100
+current_time = 0
+previous_time = 0
+
+
+# Initializing the current rocket
+current_rocket = rm.Rocket(0, 0, 0, 0, 0, 0, 0, 0, np.array([0, 0, 0]), np.array([0, 0, 0]), np.array([0, 0, 0]),
+                                    np.array([0, 0, 0]), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+
+# Getting initial params for rocket call
+def init_rocket_state(initial_rocket: rm.Rocket) -> rm.Rocket:
+    mass, thrust, burn_time = input("Enter a mass, thrust, and burn time: ").split()
+    pressure_noise, temp_noise, accel_noise, gyro_noise, mag_noise = input("Enter noise params (pressure, temperature, "
+                                                                           "acceleration, gyro, and magnetic noise):"
+                                                                           " ").split()
+    initial_rocket.mass = mass
+    initial_rocket.thrust = thrust
+    initial_rocket.burn_time = burn_time
+    initial_rocket.pressure_noise = pressure_noise
+    initial_rocket.temp_noise = temp_noise
+    initial_rocket.accel_noise = accel_noise
+    initial_rocket.gyro_noise = gyro_noise
+    initial_rocket.mag_noise = mag_noise
+    return initial_rocket
+
+
+# Updates the state of the rocket to the ground_truth and sensor_data files
+# TODO: add writing to files
+def time_update():
+    current_rocket.position_enu = rm.cartesian_to_enu_2(current_rocket)
+    current_rocket.velocity = rm.rocket_velocity(current_rocket, previous_time, current_time)
+    current_rocket.acceleration = rm.rocket_acceleration(current_rocket)
+
+    ground_truth.write("")  # Add what is being written to ground_truth file here
+    sensor_data.write("")  # Add what is being written to sensor_data file here
+
+
+# Initializing files being writen to
+with open("ground_truth.txt", 'w') as ground_truth:
+    with open("sensor_data.txt", 'w') as sensor_data:
+        headings_gt = ["Position_ENU", "Velocity", "Acceleration", "Orientation"]
+        headings_sd = ["Baro_Pressure", "Temperature", "Acceleration", "Angular_Rates", "Magnetic Field"]
+        for (heading_gt, heading_sd) in zip(headings_gt, headings_sd):
+            col_titles_gt = heading_gt.split()
+            col_titles_sd = heading_sd.split()
+            initialize_headings_gt = ' '.join(['{0: <12}'.format(title_gt) for title_gt in col_titles_gt])
+            initialize_headings_sd = ' '.join(['{0: <14}'.format(title_sd) for title_sd in col_titles_sd])
+            ground_truth.write(initialize_headings_gt + '\t')
+            sensor_data.write(initialize_headings_sd + '\t')
+
+        current_rocket = init_rocket_state(current_rocket)
+        while current_time < end_time:
+            time_update()
+            current_time += timestep
+            # print(current_rocket.__dict__) --> this is just for debugging purposes
+            current_rocket.flight_time = current_time  # TODO: fix whatever is happening here
+
+    sensor_data.close()
+ground_truth.close()
+
---
+++ rocket_math.py
@@ -0,0 +1,192 @@
+import numpy as np
+import pymap3d
+import scipy as sp
+
+
+# This file defines the Rocket class and the general math functions needed to calculate parameters for state estimation
+
+# Simulator Inputs:
+#   Rate/timestep
+#   Rocket parameters: mass, average thrust, burn time
+#   Sensor models: noise, sensitivity, bias, hysteresis, etc.
+#   Other parameters: launch tower angle, launch site altitude (
+#   we need this for later pressure calculations), local magnetic field data.
+
+# Desired calculations:
+# -Absolute position (ENU) --> East, North, Up coordinates
+# -Velocity
+# -Acceleration
+# -Orientation
+# -Barometric pressure
+# -Temperature
+# -Acceleration (x, y, z) --> need to add
+# -Angular rates (x, y, z) --> need to add
+# -Magnetic field (B field) components (x, y, z) --> need to add
+
+
+class Rocket:
+    def __init__(self, mass, thrust, burn_time, pressure_noise, temp_noise, accel_noise, gyro_noise, mag_noise,
+                 position_cart, position_enu, velocity, acceleration, orientation, baro_pressure, temperature,
+                 position_e, position_n, altitude, orien_q0, orien_q1, orien_q2, orien_q3, air_speed, flight_time):
+        self.mass = mass
+        self.thrust = thrust
+        self.burn_time = burn_time
+        self.pressure_noise = pressure_noise
+        self.temp_noise = temp_noise
+        self.accel_noise = accel_noise
+        self.gyro_noise = gyro_noise
+        self.mag_noise = mag_noise
+        self.position_cart = position_cart
+        self.position_enu = position_enu
+        self.velocity = velocity
+        self.acceleration = acceleration
+        self.orientation = orientation
+        self.baro_pressure = baro_pressure
+        self.temperature = temperature  # in Celsius
+        self.position_e = position_e
+        self.position_n = position_n
+        self.altitude = altitude
+        self.orien_q0 = orien_q0
+        self.orien_q1 = orien_q1
+        self.orien_q2 = orien_q2
+        self.orien_q3 = orien_q3
+        self.air_speed = air_speed
+        self.flight_time = flight_time
+
+
+# General assumptions made:
+# -The rocket's yaw rate is constant and zero.
+# -The rocket's pitch rate is constant and relatively small (~1 degree/s).
+# -The angle of attack is zero.
+
+# Desired calculations:
+# -Absolute position (ENU) --> East, North, Up coordinates
+# -Velocity
+# -Acceleration
+# -Orientation
+# -Barometric pressure
+# -Temperature
+# -Acceleration (x, y, z)
+# -Angular rates (x, y, z)
+# -Magnetic field (B field) components (x, y, z)
+
+
+# Temporary drag coefficient for calculating drag force
+coeff_drag = 0.3
+
+
+# Converts a distance in ft to a distance in meters (VERSION 1)
+def ft_to_meters(distance_ft) -> np.array([]):
+    for i, val in enumerate(distance_ft):
+        # print(distance_ft[i])
+        distance_ft[i] = val/3.2808
+    return distance_ft
+
+
+# (VERSION 2) Consider using this version
+def ft_to_meters_2(distance_ft) -> float:
+    return distance_ft / 3.2808
+
+
+# Converts a distance in meters to a distance in ft (VERSION 1)
+def meters_to_ft(distance_meters) -> np.array([]):
+    for i in distance_meters:
+        distance_meters[i] *= 3.2808
+    return distance_meters
+
+
+# (VERSION 2) Consider using this version
+def meters_to_ft_2(distance_meters) -> float:
+    return distance_meters * 3.2808
+
+
+# Calculating gravity based on altitude above sea level (in ft)
+def get_gravity(altitude) -> float:
+    return float(9.79121 * ((20902231 / (20902231 + altitude)) ** 2))  # Gravity constant specific to Spaceport America
+
+
+def get_vel_magnitude(current_rocket: Rocket) -> float:
+    return np.sqrt(current_rocket.velocity[0] ** 2 + current_rocket.velocity[1] ** 2 +
+                   current_rocket.velocity[2] ** 2)
+
+
+def get_vel_unit_vector(current_rocket: Rocket) -> np.array([]):
+    vel_unit_vector = [0, 0, 0]
+    for i, val in enumerate(current_rocket.velocity):
+        vel_unit_vector[i] = current_rocket.velocity[i] / get_vel_magnitude(current_rocket)
+    return vel_unit_vector
+
+# Other way of doing function:
+# return rocket.current_rocket.velocity / get_vel_magnitude(current_rocket)
+
+
+# Calculating air density from 3rd-degree polynomial based on plot of air density vs altitude data obtained from
+# https://www.engineeringtoolbox.com/standard-atmosphere-d_604.html
+def get_air_density(current_rocket: Rocket) -> float:
+    alt = current_rocket.altitude
+    return (-1.2694e-14 * (alt ** 3)) + (1.9121e-09 * (alt ** 2)) + (-8.7147e-05 * alt) + 1.1644e+00
+
+
+# Calculating the rocket's cross-sectional area. The 0.25 is the current radius (in ft) of the rocket (SOTS).
+def get_cross_sec_area() -> float:
+    temp = np.array([0.25])
+    rocket_radius = ft_to_meters(temp)
+    radius_area = rocket_radius[0]
+    return np.pi * (radius_area ** 2)
+
+
+def get_drag_force(current_rocket: Rocket) -> np.array([]):
+    drag_force = np.array([0, 0, 0])
+    drag_force_mag = -0.5 * coeff_drag * (get_vel_magnitude(current_rocket) ** 2) * get_air_density(current_rocket) * \
+                 get_cross_sec_area
+    drag_force = drag_force_mag * get_vel_unit_vector(current_rocket)
+    return drag_force
+
+
+# Calculating acceleration of the rocket (from R = T - (D + G) and F = m*a)
+# TODO: determine x,y,z acceleration
+def rocket_acceleration(current_rocket: Rocket) -> np.array([]):
+    resultant_force = np.array([0, 0, 0])
+    resultant_force[0] = current_rocket.thrust[0] - get_drag_force(current_rocket)[0]
+    resultant_force[1] = current_rocket.thrust[1] - get_drag_force(current_rocket)[1]
+    resultant_force[2] = current_rocket.thrust[2] - ((current_rocket.mass *
+                                                            get_gravity(current_rocket.altitude)) +
+                                                                        get_drag_force(current_rocket)[2])
+    current_rocket.acceleration = resultant_force / current_rocket.mass
+    return current_rocket.acceleration
+
+
+# TODO: edit this due to the fact that methodology changed
+# Calculating the current cartesian position of the rocket
+# Use this: https://apps.dtic.mil/dtic/tr/fulltext/u2/a484864.pdf
+# And this: https://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation
+def rocket_position_cartesian(current_rocket: Rocket, position_cartesian, delta_time) -> np.array([]):
+    position_cartesian = position_cartesian + (current_rocket.velocity * delta_time)
+    return position_cartesian
+
+
+# TODO: delete this once new function is done
+# Converting cartesian to enu for position of rocket
+def cartesian_to_enu(current_rocket: Rocket, position_cartesian) -> np.array([]):
+    position_cartesian = ft_to_meters(position_cartesian)
+    geodectic_coords = pymap3d.ecef2geodetic(position_cartesian[0], position_cartesian[1], position_cartesian[2], None,
+                                             True)
+    current_rocket.position_ENU = meters_to_ft(pymap3d.ecef2enu(position_cartesian[0], position_cartesian[1],
+                                                                position_cartesian[2], geodectic_coords[0],
+                                                                geodectic_coords[1], geodectic_coords[2], None, True))
+
+
+# TODO: add new function for converting from cartesian to enu
+# Converting cartesian to ENU for position of rocket
+def cartesian_to_enu_2(current_rocket: Rocket) -> np.array([]):
+    pass
+
+
+# Calculating velocity of the rocket by integrating acceleration
+# TODO: make times work in SES
+def rocket_velocity(current_rocket: Rocket, previous_time, current_time) -> np.array([]):
+    for i in current_rocket.velocity:
+        current_rocket.velocity[i] = sp.quad(current_rocket.acceleration[i], previous_time, current_time)
+    return current_rocket.velocity
+
+
