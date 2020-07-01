415eb22ac718001333b22be1154cc1768578b618
unknown <dawsonwkletke@gmail.com>
2020-07-01 17:04:35 -0400

    Adjust unit tests and remove unused functions
    In addition:
    -Some constants were changed to imperial for consistency.
    -Some general code clean up for aesthetics.

--- rocket_math.py
+++ rocket_math.py
@@ -3,8 +3,9 @@
 import numpy as np
 import pymap3d
 from math import sin, cos, radians
+
 # Using pyquaternion instead of numpy-quaternion because the later was harder to install (could change later if need be)
-#import numpy-quaternion as quat
+# import numpy-quaternion as quat
 
 
 # -----------------------CONSTANTS---------------------------
@@ -12,14 +13,18 @@ from math import sin, cos, radians
 # Temporary drag coefficient for calculating drag force
 COEFF_DRAG = 0.3
 
-# Current radius of the rocket (SOTS) [m]
+# Current radius of the rocket (SOTS) [ft]
 TUBE_RADIUS = 0.25
 
-# Gravity at ground level in Spaceport America [m/s^2]
-SA_GRAV_GROUND = 9.79121
+# Gravity at ground level in Spaceport America [ft/s^2]
+# Websites for finding the local gravity:
+# https://www.latlong.net/convert-address-to-lat-long.html
+# https://www.sensorsone.com/local-gravity-calculator/#local-gravity
+# Websites give gravity in [m/s^2], which was 9.75594. Units were converted.
+SA_GRAV_GROUND = 32.138911
 
-# Radius of the earth [m]
-RAD_EARTH = 6.371e6
+# Radius of the earth [ft]
+RAD_EARTH = 20.902e6
 
 # Mass lost per timestep [lb]
 # TODO: implement the proper loss
@@ -38,14 +43,15 @@ LOCAL_MAGNETIC_FIELD = 0  # TODO: figure out what we need to actually store here
 
 # ------------------------------------------------------------
 
+# TODO: add function definitions and remove explicit return type
 # TODO: add class definition comments
 class Rocket:
     def __init__(self, mass, thrust, burn_time, sensor_noise):
         self.mass = mass  # in pounds (lb)
         self.thrust = thrust  # in pound-force (lbf)
         self.burn_time = burn_time  # in seconds (s)
-        self.sensor_noise = sensor_noise  # not determined
-        self.position_loc_cart = np.array([0.0, 0.0, 0.0])  # in feet (ft)
+        self.sensor_noise = sensor_noise  # TODO: add in explanation for this dictionary
+        self.position_cart = np.array([0.0, 0.0, 0.0])  # in feet (ft)
         self.position_enu = np.array([0.0, 0.0, 0.0])  # in feet (ft)
         self.velocity = np.array([0.0, 0.0, 0.0])  # in feet/second (ft/s)
         self.acceleration = np.array([0.0, 0.0, 0.0])  # in feet/second^2 (ft/s^2)
@@ -55,152 +61,90 @@ class Rocket:
         self.altitude = 0.0  # in feet (ft)
         self.air_speed = 0.0  # in feet/second probably (ft/s)
 
+    # Calculates gravity based on altitude above sea level (in ft/s^2)
+    def get_gravity(self) -> float:
+        return SA_GRAV_GROUND * ((RAD_EARTH / (RAD_EARTH + self.altitude)) ** 2)
 
-# TODO: Add in units manager
-# TODO: Add precision
-# Converts a distance in feet to a distance in meters
-def ft_to_meters(distance_ft: np.array([])) -> np.array([]):
-    return distance_ft / 3.2808
+    # Calculates mass based on estimated loss in mass (in lb)
+    def get_mass(self) -> float:
+        if self.mass["comb_mass"] != 0:
+            if self.mass["comb_mass"] - MASS_LOSS < 0:
+                self.mass["comb_mass"] = 0
+                self.mass["total_mass"] = self.mass["body_mass"] + (
+                        MASS_LOSS - self.mass["comb_mass"])
+            self.mass["comb_mass"] = self.mass["comb_mass"] - MASS_LOSS
+            self.mass["total_mass"] = self.mass["body_mass"] + self.mass["comb_mass"]
+        return self.mass
 
+    # Calculates the magnitude of the velocity vector (in lbf)
+    def get_velocity_mag(self) -> float:
+        return np.sqrt(self.velocity[0] ** 2 + self.velocity[1] ** 2 + self.velocity[2] ** 2)
 
-# TODO: Add in units manager
-# TODO: Add precision
-# Converts a distance in meters to a distance in feet
-def meters_to_ft(distance_meters: np.array([])) -> np.array([]):
-    return distance_meters * 3.2808
+    # Calculates the velocity unit vector
+    # Launch angle is in degrees and is from the x-axis to the z-axis
+    # TODO: investigate numpy unit vector  (unit_vector = current_rocket.velocity / np.linalg.norm(current_rocket.velocity))
+    # TODO: fix the fact the unit vector is negative and switches sign
+    def get_velocity_uv(self) -> np.array([]):
+        if not self.get_velocity_mag(self):
+            multiplier_arr = np.array([round(cos(radians(TOWER_ANGLE)), 4), 0.0, round(sin(radians(TOWER_ANGLE)), 4)])
+            return np.array([1.0, 0.0, 1.0]) * multiplier_arr
+        return self.velocity / self.get_velocity_mag(self)
 
+    # TODO: replace this function with one from sensor measurements (temp and pressure)
+    # Calculates air density from 3rd-degree polynomial based on plot of air density vs altitude data obtained
+    # from [VERSION 1]
+    # https://www.engineeringtoolbox.com/standard-atmosphere-d_604.html
+    def get_air_density2(self) -> float:
+        alt = self.altitude
+        return (-1.2694e-14 * (alt ** 3)) + (1.9121e-09 * (alt ** 2)) + (-8.7147e-05 * alt) + 1.1644e+00
 
-# Calculates gravity based on altitude above sea level (in ft/s^2)
-def get_gravity(altitude: float) -> float:
-    return meters_to_ft(SA_GRAV_GROUND * ((RAD_EARTH / (RAD_EARTH + ft_to_meters(altitude))) ** 2))
+    # TODO: check if what the output air density and fix if the input should be in meters or feet
+    #  (possibly in meters currently)
+    # Alternative to other air density calculation [VERSION 2]
+    def get_air_density(self) -> float:
+        alt = self.altitude
+        return 1.22 * 0.9 ** (alt / 1000)
 
+    # TODO: make function into constant since no variability
+    # Calculates the rocket's cross-sectional area [ft^2]
+    def get_cross_sec_area(self) -> float:
+        return np.pi * (TUBE_RADIUS ** 2)
 
-# Calculates mass based on estimated loss in mass (in lb)
-def get_mass(current_rocket: Rocket) -> float:
-    if current_rocket.mass["comb_mass"] != 0:
-        if current_rocket.mass["comb_mass"] - MASS_LOSS < 0:
-            current_rocket.mass["comb_mass"] = 0
-            current_rocket.mass["total_mass"] = current_rocket.mass["body_mass"] + (MASS_LOSS - current_rocket.mass["comb_mass"])
-        current_rocket.mass["comb_mass"] = current_rocket.mass["comb_mass"] - MASS_LOSS
-        current_rocket.mass["total_mass"] = current_rocket.mass["body_mass"] + current_rocket.mass["comb_mass"]
-    return current_rocket.mass
+    # TODO [Later]: if speeds of rocket are transonic/supersonic, wave drag may be a thing to consider
+    # Calculates the drag force experienced by the rocket in local cartesian vector form [x,y,z] (in ft)
+    def get_drag_force(self) -> np.array([]):
+        drag_force_mag = 0.5 * COEFF_DRAG * (self.get_velocity_mag(self) ** 2) * self.get_air_density(self) * \
+                         self.get_cross_sec_area()
+        return self.get_velocity_uv(self) * drag_force_mag  # TODO: make the launch angle not hard-coded
 
+    # TODO: implement burn time
+    # Calculates the thrust vector based on velocity unit vector [x,y,z] (in lbf)
+    def get_thrust(self, current_time: float) -> np.array([]):
+        if current_time <= self.burn_time:
+            thrust_magnitude = np.sqrt(self.thrust[0] ** 2 + self.thrust[1] ** 2
+                                       + self.thrust[2] ** 2)
+            return self.get_velocity_uv(self) * thrust_magnitude  # TODO: account for launch angle
+        return np.array([0, 0, 0])
 
-# Calculates the magnitude of the velocity vector (in lbf)
-def get_vel_magnitude(current_rocket: Rocket) -> float:
-    return np.sqrt(current_rocket.velocity[0] ** 2 + current_rocket.velocity[1] ** 2 + current_rocket.velocity[2] ** 2)
+    # Calculates acceleration of the rocket in local cartesian vector form [x,y,z]
+    # TODO: check units for everything
+    # TODO: investigate time component?
+    def rocket_acceleration(self, previous_time, current_time) -> np.array([]):
+        resultant_force = np.array([0.0, 0.0, 0.0])
+        drag_force = self.get_drag_force(self)
+        resultant_force[0] = self.thrust[0] - drag_force[0]
+        resultant_force[1] = self.thrust[1] - drag_force[1]
+        resultant_force[2] = self.thrust[2] - drag_force[2] - (self.mass * self.get_gravity(self.altitude))
+        if any(resultant_force):
+            return resultant_force / self.mass
+        return self.acceleration
 
+    # TODO: add bounds checks
+    # Calculates velocity of the rocket by integrating acceleration in local cartesian vector form [x,y,z]
+    def rocket_velocity(self, previous_time, current_time) -> np.array([]):
+        return self.velocity + self.acceleration * (current_time - previous_time)
 
-# Calculates the velocity unit vector
-# Launch angle is in degrees and is from the x-axis to the z-axis
-# TODO: investigate numpy unit vector  (unit_vector = current_rocket.velocity / np.linalg.norm(current_rocket.velocity))
-# TODO: fix the fact the unit vector is negative and switches sign
-def get_vel_unit_vector(current_rocket: Rocket) -> np.array([]):
-    if not get_vel_magnitude(current_rocket):
-        print("Magnitude of velocity is 0.")
-        multiplier_arr = np.array([round(cos(radians(TOWER_ANGLE)), 4), 0.0, round(sin(radians(TOWER_ANGLE)), 4)])
-        print("This is cos: " + str(round(cos(radians(TOWER_ANGLE)), 4)))
-        print("This is sin: " + str(round(sin(radians(TOWER_ANGLE)), 4)))
-        return np.array([1.0, 0.0, 1.0]) * multiplier_arr
-    print("In get vector & This is velocity: " + str(current_rocket.velocity))
-    print("This is velocity_magnitude: " + str(get_vel_magnitude(current_rocket)))
-    return current_rocket.velocity / get_vel_magnitude(current_rocket)
-
-
-# TODO: replace this function with one from sensor measurements (temp and pressure)
-# Calculates air density from 3rd-degree polynomial based on plot of air density vs altitude data obtained
-# from [VERSION 1]
-# https://www.engineeringtoolbox.com/standard-atmosphere-d_604.html
-def get_air_density2(current_rocket: Rocket) -> float:
-    alt = current_rocket.altitude
-    return (-1.2694e-14 * (alt ** 3)) + (1.9121e-09 * (alt ** 2)) + (-8.7147e-05 * alt) + 1.1644e+00
-
-
-# TODO: check if what the output air density and fix if the input should be in meters or feet
-#  (possibly in meters currently)
-# Alternative to other air density calculation [VERSION 2]
-def get_air_density(current_rocket: Rocket) -> float:
-    alt = current_rocket.altitude
-    return 1.22 * 0.9 ** (alt / 1000)
-
-
-# TODO: make function into constant since no variability
-# Calculates the rocket's cross-sectional area [ft^2]
-def get_cross_sec_area() -> float:
-    return np.pi * (TUBE_RADIUS ** 2)
-
-
-# TODO [Later]: if speeds of rocket are transonic/supersonic, wave drag may be a thing to consider
-# Calculates the drag force experienced by the rocket in local cartesian vector form [x,y,z] (in ft)
-def get_drag_force(current_rocket: Rocket) -> np.array([]):
-    drag_force_mag = 0.5 * COEFF_DRAG * (get_vel_magnitude(current_rocket) ** 2) * get_air_density(current_rocket) * \
-                     get_cross_sec_area()
-    return get_vel_unit_vector(current_rocket) * drag_force_mag  # TODO: make the launch angle not hard-coded
-
-
-# TODO: implement burn time
-# Calculates the thrust vector based on velocity unit vector [x,y,z] (in lbf)
-def get_thrust(current_rocket: Rocket, current_time: float) -> np.array([]):
-    if current_time <= current_rocket.burn_time:
-        thrust_magnitude = np.sqrt(current_rocket.thrust[0] ** 2 + current_rocket.thrust[1] ** 2
-                                   + current_rocket.thrust[2] ** 2)
-        print("This is vel_unit_vec: " + str(get_vel_unit_vector(current_rocket)))
-        return get_vel_unit_vector(current_rocket) * thrust_magnitude  # TODO: make the launch angle not hard-coded
-    print("Made it here")
-    return np.array([0, 0, 0])
-
-
-# Calculates acceleration of the rocket in local cartesian vector form [x,y,z]
-# TODO: check units for everything
-# TODO: investigate time component?
-def rocket_acceleration(current_rocket: Rocket, previous_time: float, current_time: float) -> np.array([]):
-    resultant_force = np.array([0.0, 0.0, 0.0])
-    drag_force = get_drag_force(current_rocket)
-    resultant_force[0] = current_rocket.thrust[0] - drag_force[0]
-    resultant_force[1] = current_rocket.thrust[1] - drag_force[1]
-    resultant_force[2] = current_rocket.thrust[2] - drag_force[2] - ((current_rocket.mass *
-                                                                      get_gravity(current_rocket.altitude)) / 32.174049)
-    print("This is thrust: " + str(current_rocket.thrust[2]))
-    print("This is Fg: " + str((current_rocket.mass * get_gravity(current_rocket.altitude)) / 32.174049))
-    print("This is Fd: " + str(drag_force[2]))
-    print("This is altitude: " + str(current_rocket.altitude))
-
-    if any(resultant_force):
-        return resultant_force / current_rocket.mass
-    print("we be here :/")
-    return current_rocket.acceleration
-
-
-# TODO: edit this due to the fact that methodology changed
-# Calculates the current cartesian position of the rocket [x,y,z]
-# Use this: https://apps.dtic.mil/dtic/tr/fulltext/u2/a484864.pdf
-# And this: https://en.wikipedia.org/wiki/Quaternions_and_spatial_rotation
-def rocket_position_local_cartesian(current_rocket: Rocket, previous_time: float, current_time: float) -> np.array([]):
-    return current_rocket.position_loc_cart + current_rocket.velocity * (current_time - previous_time) + 0.5 * \
-           current_rocket.acceleration * (current_time - previous_time) ** 2
-
-
-# TODO: delete this once new function is done
-# Converting cartesian to enu for position of rocket
-def cartesian_to_enu(current_rocket: Rocket, position_cartesian) -> np.array([]):
-    position_cartesian = ft_to_meters(position_cartesian)
-    geodectic_coords = pymap3d.ecef2geodetic(position_cartesian[0], position_cartesian[1], position_cartesian[2], None,
-                                             True)
-    current_rocket.position_ENU = meters_to_ft(pymap3d.ecef2enu(position_cartesian[0], position_cartesian[1],
-                                                                position_cartesian[2], geodectic_coords[0],
-                                                                geodectic_coords[1], geodectic_coords[2], None, True))
-
-
-# TODO: add new function for converting from cartesian to enu/changing library
-# Converting local cartesian to ENU for position of rocket
-# Use this for quaternion rotation: https://kieranwynn.github.io/pyquaternion/
-def loc_cart_to_enu_2(current_rocket: Rocket, previous_time: float, current_time: float) -> np.array([]):
-    return np.array([0.0, 0.0, 0.0])
-    # rot_quaternion = Quaternion(axis=[0, 0, 0], angle=0)  # TODO: make this the true scalar and vector
-    # return rot_quaternion.rotate(current_rocket.position_loc_cart)
-
-
-# TODO: add bounds chckes
-# Calculates velocity of the rocket by integrating acceleration in local cartesian vector form [x,y,z]
-def rocket_velocity(current_rocket: Rocket, previous_time: float, current_time: float) -> np.array([]):
-    return current_rocket.velocity + current_rocket.acceleration * (current_time - previous_time)
+    # TODO: add functionality
+    # Calculated position of the rocket by integrating velocity in the local cartesian vector form [x,y,z]
+    def rocket_position(self, previous_time, current_time) -> np.array([]):
+        pass
--- tests/test_rocket_math.py
+++ tests/test_rocket_math.py
@@ -4,117 +4,185 @@ import rocket_math as rm
 import numpy as np
 
 
-def test_ft_to_meters():
-    assert rm.ft_to_meters(1) == np.array([0.3048])     # hand calc
-    assert rm.ft_to_meters(-1) == np.array([-0.3048])   # hand calc
-    assert rm.ft_to_meters(13) == np.array([3.9624])    # hand calc
-    assert rm.ft_to_meters(0) == np.array([0])
-    assert rm.ft_to_meters(2.6) == np.array([0.79248])  # hand calc
-
-
-def test_meters_to_ft():
-    assert rm.meters_to_ft(1) == np.array([3.2808])     # hand calc
-    assert rm.meters_to_ft(-1) == np.array([-3.2808])   # hand calc
-    assert rm.meters_to_ft(13) == np.array([42.6509])   # hand calc
-    assert rm.meters_to_ft(0) == np.array([0])
-    assert rm.meters_to_ft(2.6) == np.array([8.5302])   # hand calc
-
-
 def test_get_gravity():
-    assert rm.get_gravity()
+    test_rocket = rm.Rocket()
+    assert test_rocket.get_gravity()
     # skipping for now because requires hand calcs
 
 
 def test_get_mass():
+    # Test for mass above mass loss from one timestep
     mass = {"total_mass": 110, "body_mass": 55, "comb_mass": 55}
-    test_rocket = rm.Rocket(mass)
-    assert rm.get_mass(test_rocket) == 110 - rm.MASS_LOSS
-    assert rm.get_mass(test_rocket) == 110 - (2 * rm.MASS_LOSS)
+    test_rocket = rm.Rocket(mass=mass)
+    assert test_rocket.get_mass() == 110 - rm.MASS_LOSS
+    assert test_rocket.get_mass() == 110 - (2 * rm.MASS_LOSS)
     assert test_rocket.mass["comb_mass"] == 55 - (2 * rm.MASS_LOSS)
     assert test_rocket.mass["body_mass"] == 55
     assert test_rocket.mass["total_mass"] == 110 - (2 * rm.MASS_LOSS)
 
+    # Test for mass below mass loss from one timestep
     mass = {"total_mass": 100 + rm.MASS_LOSS - 0.01, "body_mass": 100, "comb_mass": rm.MASS_LOSS - 0.01}
-    test_rocket = rm.Rocket(mass)
-    assert rm.get_mass(test_rocket) == 100 - rm.MASS_LOSS - 0.01
-    assert rm.get_mass(test_rocket) == 100 - rm.MASS_LOSS - 0.01  # only 1 mass loss because comb_mass = 0
+    test_rocket.mass = mass
+    assert test_rocket.get_mass() == 100 - rm.MASS_LOSS - 0.01
+    assert test_rocket.get_mass() == 100 - rm.MASS_LOSS - 0.01  # only 1 mass loss because comb_mass = 0
     assert test_rocket.mass["comb_mass"] == 0  # 0 because the mass loss should only be subtracted twice
     assert test_rocket.mass["body_mass"] == 100
     assert test_rocket.mass["total_mass"] == 100 - rm.MASS_LOSS - 0.01  # only 1 mass loss because comb_mass = 0
 
 
 def test_get_vel_magnitude():
-    test_rocket = rm.Rocket()
-    test_rocket.velocity = np.array([0, 0, 0])
-    assert rm.get_vel_magnitude(test_rocket) == 0
+    # Test for no velocity (velocity vector is zero vector)
+    test_rocket = rm.Rocket(velocity=np.array([0, 0, 0]))
+    assert test_rocket.get_velocity_mag() == 0
 
+    # Test for velocity vector with positive integers
     test_rocket.velocity = np.array([1, 1, 1])
-    assert rm.get_vel_magnitude(test_rocket) == np.sqrt(3)
+    assert test_rocket.get_velocity_mag() == np.sqrt(3)
 
-    test_rocket.velocity = np.array([2.5, 3.5, 4.5])
-    assert rm.get_vel_magnitude(test_rocket) == np.sqrt(38.75)
-
+    # Test for velocity vector with negative integers
     test_rocket.velocity = np.array([-1, -1, -1])
-    assert rm.get_vel_magnitude(test_rocket) == np.sqrt(3)
+    assert test_rocket.get_velocity_mag() == np.sqrt(3)
+
+    # Test for velocity vector with positive floats
+    test_rocket.velocity = np.array([2.5, 3.5, 4.5])
+    assert test_rocket.get_velocity_mag() == np.sqrt(38.75)
 
 
 def test_get_vel_unit_vector():
-    test_rocket = rm.Rocket()
-    test_rocket.velocity = np.array([0, 0, 0])
-    assert rm.get_vel_unit_vector(test_rocket) == np.array([0, 0, 0])
+    # Test for no velocity (velocity vector is zero vector)
+    test_rocket = rm.Rocket(velocity=np.array([0, 0, 0]))
+    assert test_rocket.get_velocity_uv() == np.array([0, 0, 0])
 
+    # Test for velocity vector with positive integers
     test_rocket.velocity = np.array([1, 1, 1])
-    assert rm.get_vel_unit_vector(test_rocket) == np.array([(1/np.sqrt(3)), (1/np.sqrt(3)), (1/np.sqrt(3))])
+    assert test_rocket.get_velocity_uv() == np.array([(1/np.sqrt(3)), (1/np.sqrt(3)), (1/np.sqrt(3))])
 
-    test_rocket.velocity = np.array([2.5, 3.5, 4.5])
-    assert rm.get_vel_unit_vector(test_rocket) == np.array([(2.5/np.sqrt(38.75)), (3.5/np.sqrt(38.75)), (4.5/np.sqrt(38.75))])
-
+    # Test for velocity vector with negative integers
     test_rocket.velocity = np.array([-1, -1, -1])
-    assert rm.get_vel_unit_vector(test_rocket) == np.array([(-1/np.sqrt(3)), (-1/np.sqrt(3)), (-1/np.sqrt(3))])
+    assert test_rocket.get_velocity_uv() == np.array([(-1 / np.sqrt(3)), (-1 / np.sqrt(3)), (-1 / np.sqrt(3))])
+
+    # Test for velocity vector with positive floats
+    test_rocket.velocity = np.array([2.5, 3.5, 4.5])
+    assert test_rocket.get_velocity_uv() == np.array([(2.5/np.sqrt(38.75)), (3.5/np.sqrt(38.75)), (4.5/np.sqrt(38.75))])
 
 
 def test_get_air_density():
     test_rocket = rm.Rocket()
     test_rocket.altitude = 0
-    assert rm.get_air_density(test_rocket) == 23.77
+    assert test_rocket.get_air_density(test_rocket) == 23.77
     # skipping for now because requires hand calcs
 
 
-def test_get_cross_sec_area():
-    assert rm.get_cross_sec_area()
-    # skipping due to fact function is becoming a constant
-
-
 def test_get_drag_force():
-    test_rocket = rm.Rocket()
-    test_rocket.velocity = np.array([0, 0, 0])
+    # Test for no velocity (velocity vector is zero vector)
+    test_rocket = rm.Rocket(velocity=np.array([0, 0, 0]))
     test_rocket.altitude = 0  # parameter for air density
-    assert rm.get_drag_force(test_rocket) == np.array([0, 0, 0])
+    assert test_rocket.get_drag_force() == np.array([0, 0, 0])
 
+    # Test for velocity vector with positive integers
     test_rocket.velocity = np.array([1, 1, 1])
     test_rocket.altitude = 5000
-    assert rm.get_drag_force(test_rocket) == np.array([0.3482, 0.3482, 0.3482])     # hand calc
+    assert test_rocket.get_drag_force() == np.array([0.3482, 0.3482, 0.3482])     # hand calc
 
+    # Test for velocity vector with negative integers
     test_rocket.velocity = np.array([-1, -1, -1])
     test_rocket.altitude = 5000
-    assert rm.get_drag_force(test_rocket) == np.array([-0.3482, -0.3482, -0.3482])  # hand calc
+    assert test_rocket.get_drag_force() == np.array([-0.3482, -0.3482, -0.3482])  # hand calc
 
+    # Test for velocity vector with positive floats
     test_rocket.velocity = np.array([32.5, 42.5, 52.5])
     test_rocket.altitude = 10000
-    assert rm.get_drag_force(test_rocket) == np.array([0.2242, 0.2932, 0.3622])     # hand calc
+    assert test_rocket.get_drag_force() == np.array([0.2242, 0.2932, 0.3622])     # hand calc
 
 
 def test_get_thrust():
-    assert rm.get_thrust()
+    # Test for no thrust (thrust vector is zero vector)
+    test_rocket = rm.Rocket(thrust=np.array[0, 0, 0])
+    current_time = 0
+    assert test_rocket.get_thrust(current_time) == np.array([0, 0, 0])
+
+    # Test for thrust and velocity vectors with positive integers
+    test_rocket.thrust = np.array([1, 1, 1])
+    test_rocket.velocity = np.array([1, 1, 1])
+    current_time = 0
+    assert test_rocket.get_thrust(current_time) == np.array([1, 1, 1])
+
+    # Test for thrust with negative integers, velocity with positive integers
+    test_rocket.thrust = np.array([-1, -1, -1])
+    test_rocket.velocity = np.array([1, 1, 1])
+    current_time = 0
+    assert test_rocket.get_thrust(current_time) == np.array([-1, -1, -1])
+
+    # Test for current time greater than burn time
+    test_rocket.thrust = np.array([1, 1, 1])
+    test_rocket.velocity = np.array([1, 1, 1])
+    test_rocket.burn_time = 5
+    current_time = 10
+    assert test_rocket.get_thrust(current_time) == np.array([0, 0, 0])
+
+    # Test for thrust and velocity with positive floats
+    test_rocket.thrust = np.array([10.5, 11.5, 12.5])
+    test_rocket.velocity = np.array([10.5, 11.5, 12.5])
+    test_rocket.burn_time = 5
+    current_time = 4
+    assert test_rocket.get_thrust(current_time) == np.array([10.5, 11.5, 12.5])
+
+    # Need a test with random floats for thrust and velocity but can't do the hand calc rn.
 
 
 def test_rocket_acceleration():
-    assert rm.rocket_acceleration()
+    # Test for no thrust or velocity (thrust and velocity vectors are zero vector)
+    mass = {"total_mass": 110, "body_mass": 55, "comb_mass": 55}
+    thrust = np.array([0, 0, 0])
+    test_rocket = rm.Rocket(mass=mass, thrust=thrust)
+    test_rocket.velocity = np.array([0, 0, 0])
+    current_time, previous_time = 0
+    assert test_rocket.rocket_acceleration(current_time, previous_time) == -test_rocket.get_gravity()
 
+    # Test for constant acceleration/no resultant force (z component of thrust vector equals mass * gravity with no velocity)
+    mass = {"total_mass": 110, "body_mass": 55, "comb_mass": 55}
+    test_acceleration, test_rocket.acceleration = np.array([0, 0, 0])
+    thrust = np.array([0, 0, test_rocket.get_gravity()])
+    test_rocket.mass = mass
+    test_rocket.thrust = thrust
+    test_rocket.velocity = np.array([0, 0, 0])
+    current_time, previous_time = 0
+    assert test_rocket.rocket_acceleration(current_time, previous_time) == test_acceleration
 
-def test_rocket_position():
-    assert rm.rocket_position()
+    # Test for thrust and velocity with positive integers
+    mass = {"total_mass": 110, "body_mass": 55, "comb_mass": 55}
+    thrust = np.array([1, 1, 1])
+    test_rocket.mass = mass
+    test_rocket.thrust = thrust
+    test_rocket.velocity = np.array([1, 1, 1])
+    current_time, previous_time = 0
+    assert test_rocket.rocket_acceleration(current_time, previous_time) == np.array([])
+
+    # Test for thrust and velocity with with negative integers
+    mass = {"total_mass": 110, "body_mass": 55, "comb_mass": 55}
+    thrust = np.array([-1, -1, -1])
+    test_rocket.mass = mass
+    test_rocket.thrust = thrust
+    test_rocket.velocity = np.array([-1, -1, -1])
+    current_time, previous_time = 0
+    assert test_rocket.rocket_acceleration(current_time, previous_time) == np.array([])
+
+    # Test for thrust and velocity with positive floats
+    mass = {"total_mass": 110, "body_mass": 55, "comb_mass": 55}
+    test_acceleration, test_rocket.acceleration = np.array([0, 0, 0])
+    thrust = np.array([0, 0, test_rocket.get_gravity()])
+    test_rocket.mass = mass
+    test_rocket.thrust = thrust
+    test_rocket.velocity = np.array([0, 0, 0])
+    current_time, previous_time = 0
+    assert test_rocket.rocket_acceleration(current_time, previous_time) == test_acceleration
 
 
 def test_rocket_velocity():
-    assert rm.rocket_velocity()
+    test_rocket = rm.Rocket()
+    assert test_rocket.rocket_velocity()
+
+
+def test_rocket_position():
+    test_rocket = rm.Rocket()
+    assert test_rocket.rocket_position()
